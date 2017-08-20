01cdb27e7d637aa6030b00c8f94e75691ef55589
Princelo <lamkimcheung@gmail.com>
2017-08-20 20:19:19 +0100

    Creating, running, and setting the characteristics of a thread

---
+++ src/states/Calculator.java
@@ -0,0 +1,42 @@
+package states;
+
+public class Calculator implements Runnable {
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+        long current = 1L;
+        long max = 20000L;
+        long numPrimes = 0L;
+        System.out.printf("Thread '%s': START\n",
+                Thread.currentThread().getName());
+        while (current <= max) {
+            if (isPrime(current)) {
+                numPrimes++;
+            }
+            current++; }
+        System.out.printf("Thread '%s': END. Number of Primes: %d\n",
+                Thread.currentThread().getName(), numPrimes);
+    }
+
+    private boolean isPrime(long number) {
+        if (number <= 2) {
+            return true;
+        }
+        for (long i = 2; i < number; i++) {
+            if ((number % i) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
