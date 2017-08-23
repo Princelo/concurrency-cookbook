12c7c3038d56bb03a933604da37968f819768765
Princelo <lamkimcheung@gmail.com>
2017-08-23 18:11:44 +0100

    Sleeping and resuming a thread

---
+++ src/sleeping/ConsoleClock.java
@@ -0,0 +1,29 @@
+package sleeping;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class ConsoleClock implements Runnable {
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%s\n", new Date());
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                System.out.printf("The FileClock has been interrupted");
+            }
+        }
+    }
+}
